f5ad9376ecf180c08c4aa4e5191b8d559a2e0d90
Omneesh9837 <omneesh7@gmail.com>
2025-07-11 16:27:30 +0000

    dataTypes

---
+++ basic-1/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"//treat all js code as new version  but no need to type write
+
+//alert(3+3)//we are using node js not browser error
+
+//number 2 to power 53
+//bigint
+
+//string=""
+//boolean=true false
+//null=khali standalone value
+//undefined
+//symbol =unique
+
+//object 
+
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
