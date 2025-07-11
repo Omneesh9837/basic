871844b18aa42e407c0c4c58ec7e1d2b6774072a
Omneesh9837 <omneesh7@gmail.com>
2025-07-11 16:16:10 +0000

    variable learn

---
+++ basic-1/variable.js
@@ -0,0 +1,17 @@
+const accountId=1433;
+let accountEmail="myGmail";
+var password="1234"
+city="jaipur" //bina variable ke aage kuch nhi likha ho to tabh bhi hm declare kr skte h but bad practice
+
+//accountId=2 //not allowed 
+password="12"
+let state;//value not defined
+
+accountEmail="anotherEmail"
+city="agra"
+console.log(accountId);
+console.table([accountId,accountEmail,password,city,state])
+/*
+var not use because of issue in block scope
+const cannot changed
+*/ 
