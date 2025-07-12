cc37f1ec818d4d6a48b9358fda523d876d2e3ef7
Omneesh9837 <omneesh7@gmail.com>
2025-07-12 13:27:51 +0000

    dataTypeSummary

---
+++ basic-1/dataTypeSummary.js
@@ -0,0 +1,50 @@
+//data type are two type premitive and non premitive (call by vallue or call by reference age koi aur language pdhi h to)
+
+//primitive (call by value mtlb jb app copy kerte khi se copy me change hote h)
+
+//String Number Boolean Null undefined Symbol BigInt
+
+//reference (non primitive)
+
+//array objects function
+                 //define
+const score =100//number
+const scoreValue=100.3//decimel
+const isLogged=false//boolean
+const outSight=null//
+
+let userEmail //vaalue undefined
+let userEmail1=undefined
+
+//Symbol define
+
+const id =Symbol('123');
+const idDiff=Symbol('123')
+console.log(id===idDiff);
+
+const bigNumber=3455555555555555n //bigInt defien
+
+
+//array
+
+const arr=["aman","hitesh","hitesh"]
+//object
+let myobject={
+    name:"aman",
+    age:21
+}
+
+//function
+
+function myApp(){
+
+}
+const myApp1=function(){
+
+}
+const myApp2=()=>{
+
+}
+
+//typeof use for ki dataaType kya 
+console.log(typeof myObject)
